ad742851ef5993a060e2a34b94d7669052de5c15
kaigagi <kaigagipro@gmail.com>
2021-09-16 21:03:46 +0700

    add some io event

--- server.js
+++ server.js
@@ -14,14 +14,53 @@ const io = socketIO(server);
 server.listen(port);
 //handle các request để lấy file css và scipts
 app.use(express.static('public'));
+app.use(express.urlencoded({extend: true}));
 //-----------------------------------------------------------------------------------
 // handle socket.io connection
 let players = [];
+let host ;
+function checkValidName(nickname) {
+    for (let index = 0; index < players.length; index++) {
+        if (nickname == players[index]) {
+            return false
+        }
+        else
+        {
+            return true
+        }
+    }
+}
+
 io.on("connection",(socket)=>{
-    
+    console.log("a player joined")
+    io.on("joinRoom",(nickname)=>{
+        let nickname = req.body.nickname;
+        if (checkValidName(nickname)) {
+            players.push(nickname);
+        }
+        host = players[0];
+        socket.broadcast.emit("newPlayer",nickname)
+    })
+    io.on("startGame",(from)=>{
+        if (from != players[0]) {
+            return;
+        }
+        let timeStart = Date.now();
+        while (true) {
+            let now = Date.now();
+            if ((now - timeStart)>180000) {
+                break;
+            }
+        }
+    })
 })
 
 //handle some routes
 app.get("/",(req,res)=>{
     res.sendFile("index.html",{root: __dirname})
 })
+
+// app.get("/join-game",(req,res)=>{
+
+//     res.sendFile("gameRoom.html",{root: __dirname})
+// })
